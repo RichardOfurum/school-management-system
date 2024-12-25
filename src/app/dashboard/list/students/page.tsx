c7c830ccb3d30715264ddda37671db757d383263
Richard <ifeanyiiiofurum@gmail.com>
2024-12-25 09:46:31 +0100

    [Fix] fix global auth email address

--- src/app/dashboard/list/students/page.tsx
+++ src/app/dashboard/list/students/page.tsx
@@ -85,6 +85,7 @@ const renderRow = (item:Student) => (
 
 const StudentListPage = () => {
 
+  
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0 flex flex-col justify-between'>
        <div>
