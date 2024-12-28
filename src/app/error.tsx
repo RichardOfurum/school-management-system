3cdc4c4e0441506dc06cbf3b4b7e0a3e6c9fe115
Richard <ifeanyiiiofurum@gmail.com>
2024-12-28 22:34:39 +0100

    [Fix] fixed the styling of the error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const error = () => {
   return (
-    <div className='w-full bg-lamaSky flex items-center justify-center'>
+    <div className='w-full bg-lamaSky flex items-center justify-center h-screen flex-col gap-10'>
         <h1 className='text-[50px] font-extrabold'>Error Loading the page.</h1>
         <p className='text-[20px] font-thin'>Please refresh the page</p>
     </div>
